e5a1413272f0739212a6db26aac91cd2a1a20bb6
eclipseeer <eclipseeer@gmail.com>
2021-11-28 16:29:31 +0200

    Enable sending email

--- src/services/signUp/helpers/sendCodeToEmail.js
+++ src/services/signUp/helpers/sendCodeToEmail.js
@@ -1,14 +1,11 @@
 import sendgrid from '@sendgrid/mail';
 
 export const sendCodeToEmail = async (code, email) => {
-  console.log(`Sent code ${code} to ${email}`);
-  // TODO Send real email
-  return;
   await sendgrid.send({
-    to: 'volodymyr@near.foundation',
+    to: email,
     from: 'Neardrop <neardrop@linkdrop.tech>',
     subject: 'Validation code',
     text: 'validation code code code code code code v',
     html: `<table>Your validation code: <strong>${code}</strong></table>`,
   });
-}
+};
