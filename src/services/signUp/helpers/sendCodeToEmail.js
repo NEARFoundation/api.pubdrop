afddbfe3725c28180d707095e18bcc0f530ce211
eclipseeer <eclipseeer@gmail.com>
2021-11-30 16:28:56 +0200

    Update email sender

--- src/services/signUp/helpers/sendCodeToEmail.js
+++ src/services/signUp/helpers/sendCodeToEmail.js
@@ -3,7 +3,7 @@ import sendgrid from '@sendgrid/mail';
 export const sendCodeToEmail = async (code, email) => {
   await sendgrid.send({
     to: email,
-    from: 'Neardrop <neardrop@linkdrop.tech>',
+    from: 'Neardrop <hello@miami.neardrop.io>',
     templateId: process.env.SENDGRID_TEMPLATE_ID,
     dynamicTemplateData: { code },
   });
