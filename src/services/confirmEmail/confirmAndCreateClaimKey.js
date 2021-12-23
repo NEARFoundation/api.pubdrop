94b153c4ec6ee4ae02ee69a8cb940db267fb4baa
eclipseeer <eclipseeer@gmail.com>
2021-12-23 19:15:08 +0200

    Bugfix

--- src/services/confirmEmail/confirmAndCreateClaimKey.js
+++ src/services/confirmEmail/confirmAndCreateClaimKey.js
@@ -4,14 +4,14 @@ import { getED25519KeyPair } from '../../helpers/getED25519KeyPair.js';
 export const confirmAndCreateClaimKey = async (req, res, user, event) => {
   const { publicKey, secretKey } = getED25519KeyPair(user.email);
 
-  const campaign = getCampaignContract(req.near, event);
-  await campaign.add_claim_key({ args: { public_key: publicKey } });
-
   user.isConfirmed = true;
   user.confirmAttemptAt = Date.now();
   user.publicKey = publicKey;
   user.secretKey = secretKey;
   await user.save();
+
+  const campaign = getCampaignContract(req.near, event);
+  await campaign.add_claim_key({ args: { public_key: publicKey } });
 
   res.send({ publicKey, secretKey });
 };
