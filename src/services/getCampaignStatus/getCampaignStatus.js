a3df04c9133ff4d85425ea6c38839a194ec3cd5f
Oleh Ivaniuk <fedencer7@gmail.com>
2022-02-02 13:29:56 +0200

    Minor fixes

--- src/services/getCampaignStatus/getCampaignStatus.js
+++ src/services/getCampaignStatus/getCampaignStatus.js
@@ -3,7 +3,7 @@ import { checkCampaignStatus } from "./checkCampaignStatus.js";
 export const getCampaignStatus = async (req, res) => {
   try {
     const { event } = req.query;
-    res.send({ isActive: checkCampaignStatus(event, req.near) });
+    res.send({ isActive: await checkCampaignStatus(event, req.near) });
   } catch (e) {
     console.log(e);
     res.status(500).send({ error: 'Cannot check campaign status. Please try again' });
